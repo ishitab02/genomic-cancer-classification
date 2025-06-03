35771e1e6cfecfca3e0d4b132ae923d70342977f
Ishita Bhattacharyya <intransigent.sophist@gmail.com>
2025-06-03 16:04:53 +0530

    Data preprocessing updated

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,6 +1,5 @@
 import pickle
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
@@ -26,20 +25,15 @@ X_test_scaled = scaler.transform(X_test)
 X_train_scaled_df = pd.DataFrame(X_train_scaled, columns=X.columns, index=X_train.index)
 X_test_scaled_df = pd.DataFrame(X_test_scaled, columns=X.columns, index=X_test.index)
 
+# Save X_train_scaled_df for use in notebook plotting
+with open('processed/X_train_scaled_df.pkl', 'wb') as f:
+    pickle.dump(X_train_scaled_df, f)
+
 # print(X_scaled_df.shape)
 # print(X_scaled_df.head())
 
 # Calculating variance per gene to identify low-variance genes (only on training set)
 variances = X_train_scaled_df.var(axis=0)
-
-# Plotting variance distribution 
-plt.figure(figsize=(10, 5))
-plt.hist(variances, bins=100, color='red', edgecolor='black')
-plt.title("Distribution of Gene Variances After Z-Score Normalization on Training Set")
-plt.xlabel("Variance")
-plt.ylabel("Number of Genes")
-plt.grid(True)
-plt.show()
 
 # Dropping low-variance genes (variance < 0.01 based on the plot)
 variance_threshold = 0.01
