cc3813334fe3782382f0e0015585fc108ca737a3
Ishita Bhattacharyya <intransigent.sophist@gmail.com>
2025-06-03 16:08:21 +0530

    Dimensionality reduction updated

--- src/dimensionality_reduction.py
+++ src/dimensionality_reduction.py
@@ -1,6 +1,5 @@
 import pickle
 from sklearn.decomposition import PCA
-import matplotlib.pyplot as plt
 
 with open('processed/X_train_filtered.pkl', 'rb') as f:
     X_train_filtered = pickle.load(f)
@@ -16,16 +15,6 @@ cumulative_variance = pca.explained_variance_ratio_.cumsum()
 n_components_95 = (cumulative_variance >= 0.95).argmax() + 1
 print(f"Components to retain 95% variance: {n_components_95}")
 
-# Plotting cumulative explained variance
-plt.plot(cumulative_variance, marker='o')
-plt.axhline(0.95, color='red', linestyle='--')
-plt.axvline(n_components_95, color='green', linestyle='--')
-plt.title('PCA Explained Variance')
-plt.xlabel('Number of Components')
-plt.ylabel('Cumulative Variance')
-plt.grid(True)
-plt.show()
-
 # PCA transformation based on the number of components that retain 95% variance
 pca_final = PCA(n_components=n_components_95, random_state=42)
 X_train_pca = pca_final.fit_transform(X_train_filtered)
@@ -37,3 +26,6 @@ with open('processed/X_train_pca.pkl', 'wb') as f:
 
 with open('processed/X_test_pca.pkl', 'wb') as f:
     pickle.dump(X_test_pca, f)
+
+with open('processed/pca_vars.pkl', 'wb') as f:
+    pickle.dump((cumulative_variance, n_components_95), f)
